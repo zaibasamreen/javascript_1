6150c8190d4e809c57d115a0643c7430931002c4
zaibasamreen <zaibasarowar107@gmail.com>
2021-07-24 19:42:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+//javascipt is the most popular programming language.
+//It is used in frontend and backend development and also in the mobile applications.
+//It is embedded in html file using script tag.
+
+//The programming languages are categorised into two types:
+// 1. Statistically type of programming language.
+// 2. Dynamically type of programming language.
+
+// Datatype is divided into:
+// primitive datatype and referential datatype.
+
+// Primitive datatype:
+ //1. string
+// 2.number
+ //3.boolean
+// 4. null
+ // 5.undefined
+ // 6.symbol
+ 
+
+var a=3;
+console.log(a); //number
+var b="zaiba";
+console.log(b);//string
+var c=true;
+console.log(c);  //boolean
+var d=null;
+console.log(null);//null
+var e;
+console.log(e); //undefined
+
+
+console.log(typeof a);//datatype is number
+console.log(typeof b);  //datype is string
+console.log(typeof c); //datatype is boolean
+console.log(typeof d);//datatype is object
+console.log(typeof e);// datatype is undefined
+
+//Hoisting
+m=6;
+console.log(m);
+var m; //6
+
+
+
+ 
+ 
+
+ 
+
+
